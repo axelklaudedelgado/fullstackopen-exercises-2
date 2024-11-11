a216b00ce488932eb845d1cf250c77b3aa59e2b0
axelklaudedelgado <144205486+axelklaudedelgado@users.noreply.github.com>
2024-11-11 12:21:48 +0800

    fix: use static to show static content in Express

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'));
 
 morgan.token('content', function (request, response) { return JSON.stringify(request.body) })
